5f46c40dc12d1418a26aa449ac62be1d8658b1d5
unknown <eltierry@gmail.com>
2024-02-25 15:04:35 -0300

    Ajustado variaveis. Inserido intervalo de calculo do sensor ultrassonico. Definido pinMode.

--- src/calculoSensorUltrassonico.cpp
+++ src/calculoSensorUltrassonico.cpp
@@ -3,32 +3,35 @@
 #include <PubSubClient.h>
 #define trig 18 // Pino 9 do Arduino será a saída de trigger
 #define echo 19 // Pino 8 do Arduino será a entrada de echo
-float pulse;    // Variável que armazena o tempo de duração do echo
-double dist_cm; // Variável que armazena o valor da distância em centímetros
+unsigned long pulse;    // Variável que armazena o tempo de duração do echo
+double dist_cm;  // Variável que armazena o valor da distância em centímetros
 
 StaticJsonDocument<200> doc;
-static unsigned long delayEnvia;
+static unsigned long proximoCalculo;
 
 String trigPulse()
 {
-    char out[128];
+    pinMode(trig, OUTPUT); // Pino de trigger será saída digital
+    pinMode(echo, INPUT);  // Pino de echo será entrada digital
+    if (millis() - proximoCalculo > 2000)
+    {
+        char out[128];
+        digitalWrite(trig, HIGH);             // Pulso de trigger em nível alto
+        delayMicroseconds(10);              // duração de 10 micro segundos
+        digitalWrite(trig, LOW);              // Pulso de trigge em nível baixo
+        pulse = pulseIn(echo, HIGH, 200000); // Mede o tempo em que o pino de echo fica em nível alto
 
-    char msg_out1[20];
-    char msg_out2[20];
-    digitalWrite(trig, HIGH); // Pulso de trigger em nível alto
-    delayMicroseconds(10);    // duração de 10 micro segundos
-    digitalWrite(trig, LOW);  // Pulso de trigge em nível baixo
+        dist_cm = (unsigned long)(pulse * 100 / 58.82) / 100.0; // Valor da distância em centímetros
 
-    pulse = pulseIn(echo, HIGH, 200000); // Mede o tempo em que o pino de echo fica em nível alto
-    //dist_cm = pulse / 58.82;             // Valor da distância em centímetros
+        //float nivelPorCento = (float)(map((long)(dist_cm * 100), 6600, 2300, 010, 10000)) / 100; // Cada número foi adicionado dois dígitos para ter as casas decimais
+        doc["distanciaCaixa01"] = dist_cm;                                                       // pulse / 58.82;
+        doc["nivelCaixa01"] = (double)(map((long)(dist_cm * 100), 6600, 2300, 010, 10000)) / 100; // nivelPorCento;                                                     //(double)(map((long)(dist_cm * 100), 6600, 2300, 010, 10000)) / 100;
 
-    // dtostrf(dist_cm, 2, 2, msg_out1);
-    //  MQTT.publish(TOPIC_PUBLISH, msg_out1);
+        serializeJson(doc, out);
 
-    // double nivelPorCento = (double)(map((long)(dist_cm * 100), 6600, 2300, 010, 10000)) / 100; // Cada número foi adicionado dois dígitos para ter as casas decimais
-    doc["distanciaCaixa01"] = pulse / 58.82;
-    doc["nivelCaixa01"] = (double)(map((long)(dist_cm * 100), 6600, 2300, 010, 10000)) / 100;
-    
-    serializeJson(doc, out);
-    return out;
+        proximoCalculo = millis();
+
+        return out;
+    }
+    return emptyString;
 }
